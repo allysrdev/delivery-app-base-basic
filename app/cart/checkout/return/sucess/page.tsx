9ba39094464ba9f5f838e7e6056d8d758aedf73b
Ally Santana <contato.allysantana@gmail.com>
2025-03-16 02:05:46 -0300

    ++

--- app/cart/checkout/return/sucess/page.tsx
+++ app/cart/checkout/return/sucess/page.tsx
@@ -1,32 +1,36 @@
 'use client'
 import { useCart } from '@/app/context/CartContext';
-import { Button } from '@/components/ui/button';
 import { createOrder } from '@/services/orderService';
 import { getUser, User } from '@/services/userService';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image'
-import { redirect } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 function Page() {
-  const { cart } = useCart();
+    const { cart } = useCart();
     const [user, setUser] = useState<User | null>(null);
     const session = useSession();
+    const [step, setStep] = useState<number>()
   
   
-    useEffect(() => { 
+  useEffect(() => { 
+    setStep(0)
+    
       const fetchUser = async () => {
         try {
           const userData = await getUser(session.data?.user?.email || '');
           setUser(userData);
+          setTimeout(() => setStep(1), 2000)
         } catch (error) {
           console.error("Error fetching user data:", error);
         }
+        handleCreateOrder()
       };
-      fetchUser();
-    })
+    fetchUser();
+    }, [session.data?.user?.email])
   
   function handleCreateOrder() {
+
     const items = cart.map((item) => ({
         id: item.id,
         quantity: item.quantity,
@@ -34,17 +38,46 @@ function Page() {
         name: item.name
       }));
     createOrder(user?.userId || '', user?.telephone || '', user?.name || '', user?.email || '', user?.address || '', items, (cart.reduce((acc, product) => acc + product.price * product.quantity, 0) + 10), 'Pedido solicitado')
-    redirect('/pedidos');
+    setTimeout(() => setStep(2), 2000)
+
   }
   
   return (
     <div className='flex flex-col w-full h-[90vh] overflow-hidden items-center justify-center gap-4'>
       <div className='flex flex-col justify-center items-center'>
-        <Image src={'/credit-card.png'} width={80} height={80} alt=''/>
-        <h1>Pagamento aprovado!</h1>
+        {
+          step === 0 ? (
+            <>
+            <Image src={'/credit-card.png'} width={40} height={40} alt=''/>
+              <br />
+              <p>......</p>
+              <br />
+            <h1>Pagamento aprovado!</h1>
+            </>
+      )
+            : step === 1 ? (
+              <>
+      <Image src={'/delivery.png'} width={40} height={40} alt='' />
+             <br />
+              <p>...............</p>
+              <br />
+                <h1>Processando pedido</h1>
+      
+              </>
+      ) :
+              (
+                <>
+        <Image src={'/shopping-bag.png'} width={40} height={40} alt='' />
+             <br />
+              <p>..............................</p>
+              <br />
+                  <h1>Aguardando confirmação da loja</h1>
+                </>
+      
+      )
+        } 
       </div>
-      <Button onClick={handleCreateOrder}>Acompanhar pedido</Button>
-    </div>
+      </div>
   )
 }
 
